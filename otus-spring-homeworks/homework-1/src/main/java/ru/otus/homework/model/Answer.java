4905ebbfdd83eee8d6b7f3dc3e7243dc6156cd00
Kirill <kirillzav@mail.ru>
2022-12-12 22:17:06 +0300

    ref: зменение структуры проекта

--- otus-spring-homeworks/homework-1/src/main/java/ru/otus/homework/model/Answer.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.homework.model;
-
-import lombok.Data;
-
-@Data
-public class Answer<T> {
-    private T title;
-}
--- otus-spring-homeworks/homework-1/src/main/java/ru/otus/homework/model/Question.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.homework.model;
-
-import lombok.Data;
-
-@Data
-public class Question<T> {
-    private T title;
-}
